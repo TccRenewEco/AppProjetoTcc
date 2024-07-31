6efeb0a073ce35ca0a04cca1ac55238428725c32
thales <thales.licinio05@gmail.com>
2024-07-31 15:31:03 -0300

    Botões novos adicionados

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,31 @@
 import { useNavigate } from "react-router-dom"
 
 
+
 export function TopMenu(){
    const NaviM = useNavigate()
     return(
         <header>       
-        <div id="menubar">
-        <h3><a href="#">Teste</a></h3>
-             <ul>
-               
-               <li><a onClick={()=>{NaviM('/pag1')}}>página1</a></li>
-               <li><a onClick={()=>{NaviM('/pag2')}}>página2</a></li>
-               <li><a onClick={()=>{NaviM('/pag3')}}>Página3</a></li>
+         <div id="menubar" >
+          <h3>
+            <a href="#">RenewEco</a>
+          </h3>
+             <ul className="AreaBtn">
+               <li><a onClick={()=>{NaviM('/pag1')}}>Soluções</a></li>
+               <li><a onClick={()=>{NaviM('/pag2')}}>Sobre</a></li>
+               <li><a onClick={()=>{NaviM('/pag3')}}>Contato</a></li>
+           
+             {/*Arrumar uma forma de deixar a Nav editavel com atributos*/}
+               <div id="AreaUsu">
+                <li><a href="">notificação</a></li>
+                <li><a href="">Log-in</a></li>
+                <li><a href="">Cadastro</a></li>
+               </div>
              </ul>
-        </div>
+         </div>
         </header>
+
+        
     )
 
 }
--- src/pages/pag3.tsx
+++ src/pages/pag3.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-
+import './../App.css'
 export function Testpa3(){
 
     const navi = useNavigate()
