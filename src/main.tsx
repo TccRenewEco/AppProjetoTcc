06f8118d0e144bb0fac75d44bb82318a92656c45
ManoPedra <thales.licinio05@gmail.com>
2024-08-14 14:13:48 -0300

    implementando biblioteca Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import {ChakraProvider} from '@chakra-ui/react'
 import './Styles/index.css'
 import { RotaMestre } from './routes.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RotaMestre/>
+    <ChakraProvider>
+     <RotaMestre/>
+    </ChakraProvider>
   </React.StrictMode>,
 )
